4bd2d7ff425261501fd75fd3be3a13b1e3f4bee9
Unknown <lmungovan@yahoo.com>
2018-02-07 17:34:55 -0500

    Auto stash before merge of "master" and "origin/master"

--- main.c
+++ main.c
@@ -1,22 +1,42 @@
 /* $begin shellmain */
 /*#include "csapp.h" */
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+#include <setjmp.h>
+#include <signal.h>
+#include <dirent.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <errno.h>
+#include <math.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 #define MAXARGS   128
 
 int main() 
 {
     char cmdline[MAXLINE]; /* Command line */
-
-    while (1) 
-	{
-		/* Read */
-		printf("> ");                   
-		//Fgets(cmdline, MAXLINE, stdin); 
-		if (feof(stdin))
-		{
-	    	exit(0);
-		}
+    char *Fgets(char *ptr, int n, FILE *stream);
+    while (1) {
+	/* Read */
+	printf("> ");                   
+	Fgets(cmdline, MAXLINE, stdin); 
+	if (feof(stdin))
+	    exit(0);
 	}
 }
 
