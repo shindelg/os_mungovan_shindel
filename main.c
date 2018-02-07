ebe883dc338fdaa7aef5a8a030d56cd6a51b7324
Unknown <greg@shindel.com>
2018-02-07 17:36:50 -0500

    cleanup includes

--- main.c
+++ main.c
@@ -1,29 +1,6 @@
 /* $begin shellmain */
 /*#include "csapp.h" */
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <unistd.h>
-#include <string.h>
-#include <ctype.h>
-#include <setjmp.h>
-#include <signal.h>
-#include <dirent.h>
-#include <sys/time.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <sys/mman.h>
-#include <errno.h>
-#include <math.h>
-#include <pthread.h>
-#include <semaphore.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 
 #define MAXARGS   128
 
