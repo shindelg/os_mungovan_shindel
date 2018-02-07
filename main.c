80a4fd1189c08b2d734b489bc354f7538ae53cbe
Unknown <greg@shindel.com>
2018-02-07 16:47:30 -0500

    updated main, added main.h

--- main.c
+++ main.c
@@ -1,3 +1,23 @@
-/* test
+/* $begin shellmain */
+/*#include "csapp.h" */
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include "main.h"
 
-/* test from git
+#define MAXARGS   128
+
+int main() 
+{
+    //char cmdline[MAXLINE]; /* Command line */
+
+    while (1) {
+	/* Read */
+	printf("> ");                   
+	//Fgets(cmdline, MAXLINE, stdin); 
+	if (feof(stdin))
+	    exit(0);
+	}
+}
