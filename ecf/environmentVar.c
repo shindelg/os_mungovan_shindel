5f3faed605bd8328bd977b40226aec141b44a75c
Unknown <greg@shindel.com>
2018-02-17 14:50:42 -0500

    Added memory cleanup

--- ecf/environmentVar.c
+++ ecf/environmentVar.c
@@ -105,85 +105,9 @@ void echo_var(char ** argv)
     	printf("%s\n", " not found in environment.");
     }
 
+    free(variableName);
+    free(variableValue);
     return;
-
-
-	/*
-	pid_t pid;
-	int status; 
-
-	//extern char ** environ;
-	
-	char * varCopy;
-	//If no environmental variable following echo
-	if(argv[1] == NULL)
-	{
-		printf("%s\n", "Echo requires two arguments");
-		return;
-	}
-	//remove $ from beginning of var to find. 
-	//ex: $PATH to find PATH variable, search using PATH
-	
-	
-	if(varCopy[1] == '$' && !(varCopy == NULL)) // should this be strcmp???
-		//printf("%s\n", varCopy);
-		memmove(varCopy, varCopy + 1, strlen(varCopy));
-		argv[1] = varCopy; 
-	
-	//printf("%s\n", "calling execvp");
-
-	pid = fork();
-	if(pid == 0) // If the child
-	{
-		printf("%s", "argv[0]: ");
-		printf("%s\n", argv[0]);
-
-		printf("%s", "argv[1]: ");
-		printf("%s\n", argv[1]);
-
-		status = execvp(*argv, argv);
-
-		if(status < 0)
-		{
-			printf("%s", "Unable to call system command: " );
-			printf("%s\n", argv[0]);
-			exit(0);
-		}
-	}
-	else
-	{
-		//In parent process, wait for child to finish.
-		if (waitpid(pid, &status, 0) < 0)
-			printf("%d %s", pid, "This was the unix error");
-		return;
-	}
-
-	return;
-
-	/*
-	//Print the variable value if it exists, blank if not
-	int i = 0; 
-	char copy[1000];
-	char * token;
-	char * environHold;
-	while(environ[i])
-	{
-		environHold = environ[i];
-		//Split the environment variable by =
-		strcpy(copy, environHold);
-		token = strtok(copy, "=");
-
-		if(!strcmp(token, varCopy))
-		{
-			printf("%s\n", environHold);
-			return(1);
-		}		
-		//If the first value = the input envVar, print, else continue
-		i++;
-	}
-
-	printf("%s\n", "");
-	return(1); */
 } 
 
 char * split_str(char * string)
